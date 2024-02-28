63e10fca9524047fdf1a93bc1ac2db4c0e0a9d01
Emre Sari <emre.sari@iproov.com>
2024-02-28 15:23:52 +0000

    Move SDKVersionLabel to the bottom

--- ExampleAppMAUI/MainPage.xaml.cs
+++ ExampleAppMAUI/MainPage.xaml.cs
@@ -23,7 +23,7 @@ public partial class MainPage : ContentPage, IProovWrapper.IStateListener
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        SDKversion.Text = wrapper.GetSdkVersion();
+        SDKVersionLabel.Text = "SDK Version:" + wrapper.GetSdkVersion();
         assuranceType = AssuranceType.GenuinePresence;
         claimType = ClaimType.Enrol;
     }
